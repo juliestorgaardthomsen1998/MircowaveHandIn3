407e1608099ff58fa78485067cd7c59289f9222f
Julie Thomsen <78859162+juliestorgaardthomsen1998@users.noreply.github.com>
2022-04-07 12:25:41 +0200

    Add project files.

---
+++ HandIn3Microwave/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HandIn3Microwave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
