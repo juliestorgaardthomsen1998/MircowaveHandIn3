3e079bad8e78e12173a057a90341deb8de953d3f
Julie Thomsen <78859162+juliestorgaardthomsen1998@users.noreply.github.com>
2022-04-21 13:33:21 +0200

    Ændret i metoden ChangeTime inde i Timer klassen.

--- HandIn3Microwave/Program.cs
+++ HandIn3Microwave/Program.cs
@@ -50,10 +50,10 @@ namespace HandIn3Microwave
                 switch (Console.ReadKey(true).KeyChar)
                 {
                     case '+':
-
+                        timer.ChangeTime("+");
                         break;
                     case '-':
-
+                        timer.ChangeTime("-");
                         break;
                     //case '':
                     //    break;
